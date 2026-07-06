756eddf624e88189892512e0449c40b044b6b333
Doménica Bolaños <dbolanos@espol.edu.ec>
2026-07-06 19:01:10 +0000

    cambio

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Grid } from '@mui/material';
+
+function App() {
+   return (
+      <Grid>
+
+         {/* Encabezado */}
+         <Grid>Elemento: Encabezado</Grid>
+
+         {/* Alertas */}
+         <Grid>Elemento: Alertas</Grid>
+
+         {/* Selector */}
+         <Grid>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid>Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid>Elemento: Gráfico</Grid>
+
+         {/* Tabla */}
+         <Grid>Elemento: Tabla</Grid>
+
+         {/* Información adicional */}
+         <Grid>Elemento: Información adicional</Grid>
+
+      </Grid>
+   );
+}
