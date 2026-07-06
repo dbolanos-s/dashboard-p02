83ccfd6aa8312ac01ed87fb7fc3bb5d81279d5a9
Doménica Bolaños <dbolanos@espol.edu.ec>
2026-07-06 19:19:34 +0000

    Estructura de Grid responsiva con MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
-import { Grid } from '@mui/material';
+import Grid from '@mui/material/Grid';
 
 function App() {
-   return (
-      <Grid>
+  return (
+    <Grid container spacing={5} sx={{ justifyContent: "left", alignItems: "center" }}>
 
-         {/* Encabezado */}
-         <Grid>Elemento: Encabezado</Grid>
+      {/* Encabezado */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
 
-         {/* Alertas */}
-         <Grid>Elemento: Alertas</Grid>
+      {/* Alertas */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
 
-         {/* Selector */}
-         <Grid>Elemento: Selector</Grid>
+      {/* Selector */}
+      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
 
-         {/* Indicadores */}
-         <Grid>Elemento: Indicadores</Grid>
+      {/* Indicadores */}
+      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
-         {/* Gráfico */}
-         <Grid>Elemento: Gráfico</Grid>
-
-         {/* Tabla */}
-         <Grid>Elemento: Tabla</Grid>
-
-         {/* Información adicional */}
-         <Grid>Elemento: Información adicional</Grid>
-
+      {/* Gráfico */}
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>
+        Elemento: Gráfico
       </Grid>
-   );
+
+      {/* Tabla */}
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>
+        Elemento: Tabla
+      </Grid>
+
+      {/* Información adicional */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+    </Grid>
+  );
 }
+
+export default App;
